e534afe7d4d0c90ada780ea961137ffed149f500
SailingChen10 <sailinchengo@gmail.com>
2026-02-28 21:53:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser'
+    assetsDir: 'assets'
   }
 })
